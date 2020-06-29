e91fae59b45b5e1edb98bb3191151216ab2911d3
The-Napster <connorh16@gmail.com>
2020-06-29 17:19:55 -0400

    Made major changes and additions.

--- assets/java/app.js
+++ assets/java/app.js
@@ -1,3 +1,10 @@
-$(document).ready(function () {
-    $('.parallax').parallax();
+var ready = (callback) => {
+    if (document.readyState != "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+}
+
+ready(() => {
+    M.Parallax.init(document.querySelectorAll(".parallax"));
 });
+
+
